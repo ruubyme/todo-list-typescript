46620f7df18fecc0c2b60791e32f85f299edee84
ruubyme <wngml1741@naver.com>
2024-01-26 10:51:51 +0900

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,24 @@
 import TodoList from "../components/TodoList";
 import { TodosContextProvider } from "../Contexts/TodosContext";
+import axios from "axios";
+import { useState } from "react";
 
 const Home: React.FC = () => {
+  const [test, setTest] = useState<string>("");
+  const testfunction = async () => {
+    const response = await axios.get(
+      "https://flask-production-5ca5.up.railway.app/print"
+    );
+    setTest(response.data);
+  };
+
   return (
-    <TodosContextProvider>
-      <TodoList />
-    </TodosContextProvider>
+    <>
+      <TodosContextProvider>
+        <TodoList />
+      </TodosContextProvider>
+      <h1>{test}</h1>
+    </>
   );
 };
 
