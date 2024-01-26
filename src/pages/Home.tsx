e8fd1f9038f8eb45ce3f76525043eac17919f6e5
ruubyme <wngml1741@naver.com>
2024-01-26 11:49:38 +0900

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 const Home: React.FC = () => {
   const [test, setTest] = useState<string>("");
   const testfunction = async () => {
-    const response = await axios.get("http://0.0.0.0:7009/print");
+    const response = await axios.get("https://0.0.0.0:7009/print");
     setTest(response.data);
   };
 
